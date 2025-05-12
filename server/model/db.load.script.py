7dab5c3b993c1c4aadc11397464162b56a140f87
Shahar <shahar.g@myzorro.co>
2025-05-12 14:07:29 +0300

    ref: wip

--- server/model/db.load.script.py
+++ server/model/db.load.script.py
@@ -1,4 +1,4 @@
-# this script will read the users 
+# this script will read the users
 from dotenv import load_dotenv
 import pymongo
 from pymongo import MongoClient
@@ -19,6 +19,8 @@ client = MongoClient(MONGO_URL)
 db = client["hermes"]
 user_collection = db["users"]
 articles_collection = db["articles"]
+likes_collection = db["likes"]
+
 
 def load_users():
     """
@@ -30,32 +32,51 @@ def load_users():
 
     user_collection.insert_many(users_dict)
 
+
 def load_articles():
     """
     Load articles from .tsv and load to db.
     """
     articles = pd.read_csv("data/news.tsv", sep="\t")
 
-    print(articles.iloc[:,0].head()) # article_id
-    print(articles.iloc[:,1].head()) # genre
-    print(articles.iloc[:,2].head()) # topic
-    print(articles.iloc[:,3].head()) # title
-    print(articles.iloc[:,4].head()) # subtitle
-    print(articles.iloc[:,5].head()) # url
-    
+    print(articles.iloc[:, 0].head())  # article_id
+    print(articles.iloc[:, 1].head())  # genre
+    print(articles.iloc[:, 2].head())  # topic
+    print(articles.iloc[:, 3].head())  # title
+    print(articles.iloc[:, 4].head())  # subtitle
+    print(articles.iloc[:, 5].head())  # url
+
     articles = articles.iloc[:, 0:6]
-    articles.columns = ["article_id", "genre", "topic", "title", "subtitle", "url"]
+    articles.columns = ["article_id", "genre",
+                        "topic", "title", "subtitle", "url"]
     articles = articles.drop_duplicates(subset=["article_id"])
     articles = articles.reset_index(drop=True)
-    
+
     # Convert the DataFrame to a list of dictionaries
     articles_dict = articles.to_dict(orient="records")
-    print(articles_dict[0]) # print the first article
+    print(articles_dict[0])  # print the first article
     # Insert the articles into the MongoDB collection
     articles_collection.insert_many(articles_dict)
-    
+
+
+def load_action_history():
+    """
+    Load action history from .tsv and load to db.
+    """
+    actions = pd.read_csv("data/behaviors.tsv", sep="\t")
+    actions = actions.iloc[:, 1:4]
+    actions.columns = ["user_id", "time_stamp", "article_id"]
+    actions = actions.reset_index(drop=True)
+    actions['article_id'] = actions['article_id'].str.split()
+    actions = actions.explode('article_id').reset_index(drop=True)
+    actions['time_stamp'] = pd.to_datetime(
+        actions['time_stamp'], format='%m/%d/%Y %I:%M:%S %p')
+    actions_dict = actions.to_dict(orient="records")
+
+    print(len(actions_dict))  # print the number of actions``
+    # likes_collection.insert_many(actions_dict)
+
+
 if __name__ == "__main__":
-    # Load users
     # load_users()
-    # Load articles
-    load_articles()
+    load_action_history()
